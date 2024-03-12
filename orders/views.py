fd952b6c15ac4560c4612a87b794863329afa204
arsalwn <amir.arsalan.1381a@gmail.com>
2024-03-12 13:40:33 +0330

    'order '

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .forms import OrderForm
+from cart.cart import Cart
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext as _
+
+from .models import OrderItem
 
 
+@login_required
 def order_create(request):
-    return render(request, 'orders/order_create.html')
+    order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('your cart is empty'))
+        return redirect('product-list')
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+
+        if order_form.is_valid():
+            order_obj = order_form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_obj,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+
+                )
+            cart.clear_all()
+            messages.success(request, _('your order is successfully '))
+            request.user.first_name = order_obj.first_name
+            request.user.last_name = order_obj.last_name
+            request.user.save()
+    return render(request, 'orders/order_create.html',
+                  context={'form': order_form})
